574f53e9dd3d248a37d61b718b6873d15fa26a11
yoonwonsik160317 <wssesang@gmail.com>
2024-08-19 17:45:32 +0900

    8월19일 커밋

--- web0802/src/main/java/driver/Ex1.java
+++ web0802/src/main/java/driver/Ex1.java
@@ -6,9 +6,10 @@ import board.BoardDTO;
 public class Ex1 {
 
 	public static void main(String[] args) {
-		BoardDTO dto = new BoardDTO(17, "최하나", "최의 글 제목", "최의 글 내용", null, 0);
+		BoardDTO dto = new BoardDTO(17, "최하나2", "최의 글 제목2", "최의 글 내용2", null, 0);
 		BoardDAO dao = new BoardDAO();
 		dao.updateBoard(dto);
+		
 	}
 
 }
